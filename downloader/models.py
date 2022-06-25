f07d8f83444d823af09282e134a0bb10d4cfc5ff
miirzamiir <miirzamiir@gmail.com>
2022-06-25 16:11:22 +0430

    created models.

--- downloader/models.py
+++ downloader/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+    
+class DownloadRequest(models.Model):
 
-# Create your models here.
+    FILE_TYPE_CHOICES = [
+        ('A', 'Audio'),
+        ('L', 'Low Quality Video'),
+        ('H', 'High Quality Video')
+    ]
+
+    link = models.CharField(max_length=255)
+    video_id = models.CharField(max_length=15)
+    requested_at = models.DateTimeField(auto_now=True)
+    expire_time = models.DateTimeField()
+    file_type = models.CharField(max_length=2, choices=FILE_TYPE_CHOICES)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.video_id + '_' + self.requested_at
